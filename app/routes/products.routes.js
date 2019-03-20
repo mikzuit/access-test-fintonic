05f8ff067e2d1d43ae11ea80ca925a0da80f625a
Miguel Ruiz(lw) <ma.ruiz.j@hotmail.com>
2019-03-20 16:12:19 +0100

    secure POST and DELETE with authentication

--- app/routes/products.routes.js
+++ app/routes/products.routes.js
@@ -5,14 +5,25 @@
 module.exports = (app) => {
     const products = require("../controllers/products.controller.js");
 
-    //New Product
-    app.post("/products", products.create);
+    // TODO : jwt implementation
+    // Check for authentication
+    const auth = (req, res, next) => {
+        if(req.headers.token === "fintonic") {
+            return next();
+        } else {
+            res.status(403);
+            return next(new Error("Not authorized"));
+        }
+    }
+
+    //New Product, needs authentication
+    app.post("/products", auth, products.create);
 
     //List Products
     app.get("/products", products.findAll);
 
-    //Delete Product
-    app.delete("/products/:productId", products.delete);
+    //Delete Product,needs authentication
+    app.delete("/products/:productId", auth, products.delete);
 
     // TODO : getById and Put routes
 }
